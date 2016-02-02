cc498c0a52eee64cbad1ca83555289b05b14ca5c
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-02 23:25:04 +0530

    Added ButterKnife Dependency and reduced the lines of code

--- app/src/main/java/com/ladwa/aditya/udacity/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/udacity/MainActivity.java
@@ -1,8 +1,6 @@
 package com.ladwa.aditya.udacity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,32 +9,37 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    Button btnSpotify, btnScores, btnLibrary, btnBuildItBigger, btnXyz, btnCapStone;
+public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.spotifystreamer)
+    Button btnSpotify;
+    @Bind(R.id.scoreapp)
+    Button btnScores;
+    @Bind(R.id.libraryapp)
+    Button btnLibrary;
+    @Bind(R.id.builditbigger)
+    Button btnBuildItBigger;
+    @Bind(R.id.xyzreader)
+    Button btnXyz;
+    @Bind(R.id.capstone)
+    Button btnCapStone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Declare buttons
-        btnSpotify = (Button) findViewById(R.id.spotifystreamer);
-        btnScores = (Button) findViewById(R.id.scoreapp);
-        btnLibrary = (Button) findViewById(R.id.libraryapp);
-        btnBuildItBigger = (Button) findViewById(R.id.builditbigger);
-        btnXyz = (Button) findViewById(R.id.xyzreader);
-        btnCapStone = (Button) findViewById(R.id.capstone);
 
-        //Set on Click listeners
-        btnSpotify.setOnClickListener(this);
-        btnScores.setOnClickListener(this);
-        btnLibrary.setOnClickListener(this);
-        btnBuildItBigger.setOnClickListener(this);
-        btnXyz.setOnClickListener(this);
-        btnCapStone.setOnClickListener(this);
     }
 
     @Override
@@ -61,32 +64,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.spotifystreamer:
-                showToast(getString(R.string.spotify_streamer));
-                break;
-            case R.id.scoreapp:
-                showToast(getString(R.string.scores_app));
-                break;
-            case R.id.libraryapp:
-                showToast(getString(R.string.library_app));
-                break;
-            case R.id.builditbigger:
-                showToast(getString(R.string.build_it_bigger));
-                break;
-            case R.id.xyzreader:
-                showToast(getString(R.string.xyz_reader));
-                break;
-            case R.id.capstone:
-                showToast(getString(R.string.capstone));
-                break;
+    @OnClick({R.id.spotifystreamer, R.id.scoreapp, R.id.libraryapp, R.id.builditbigger, R.id.xyzreader, R.id.capstone})
+    public void showToast(Button b) {
 
-        }
-    }
-
-    public void showToast(CharSequence s) {
-        Toast.makeText(getApplicationContext(), getString(R.string.button_launch) + " " + s.toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), getString(R.string.button_launch) + " " + b.getText(), Toast.LENGTH_SHORT).show();
     }
 }
